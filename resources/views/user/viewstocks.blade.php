09dd3a51cee9f23abbc721a11d031b978bda6a9c
abbbhucho <abcbhucho@gmail.com>
2019-07-02 13:00:26 +0530

    new changes in viewstock

--- resources/views/user/viewstocks.blade.php
+++ resources/views/user/viewstocks.blade.php
@@ -27,12 +27,12 @@
                    </thead>
     <tbody>
     
-        
+    <?php $c = 0;?>
         
             @if(count($views) > 0)
                 @foreach($views as $view)
-                <?php $c = 0;
-                $c++; ?>
+                    <?php $c++; ?>
+                 
         <tr class="post{{$view->id}}">
                     <td>{{ $c }}</td>
                     <td>{{ $view->stock_name }}</td>
@@ -42,22 +42,9 @@
                     
                     
                     <td><p data-placement="top" data-toggle="tooltip" title="View"><a href="#" class="show-modal btn btn-info btn-xs" data-toggle="modal" data-target="#viewModal{{$view->id}}"><i class="fas fa-eye"></i></a></td>
-                    <td><p data-placement="top" data-toggle="tooltip" title="Edit"><a href="#" class="edit-modal btn btn-primary btn-xs" data-id="{{$view->id}}" data-name="{{$view->stock_name}}" data-quantity="{{ $view->buy_quantity }}" data-price = "{{ $view->buy_quantity }}" data-toggle="modal" data-target="#editModal{{$view->id}}"><i class="fas fa-edit"></i></a></td>
-                    <td><p data-placement="top" data-toggle="tooltip" title="Sell"><a href="#" class="edit-modal btn btn-warning btn-xs" data-id="{{$view->id}}" data-name="{{$view->stock_name}}" data-quantity="{{ $view->buy_quantity }}" data-price = "{{ $view->buy_quantity }}" data-toggle="modal" data-target="#sellModal{{$view->id}}"><i class="fas fa-dollar-sign"></a></td>
-                    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><a href="#" class="show-modal btn btn-danger btn-xs" data-id="{{$view->id}}" data-name="{{$view->stock_name}}" data-quantity="{{ $view->buy_quantity }}" data-price = "{{ $view->buy_quantity }}" data-toggle="modal" data-target="#deleteModal{{$view->id}}"><i class="fas fa-trash"></i></a></td>
-                     
-                    
-                   
-        </tr>
-
-   
-    
-   
-    
-
-       
-            </div>
-            
+                    <td><p data-placement="top" data-toggle="tooltip" title="Edit"><a href="#" class="edit-modal btn btn-primary btn-xs" data-id="{{$view->id}}" data-toggle="modal" data-target="#editModal{{$view->id}}"><i class="fas fa-edit"></i></a></td>
+                    <td><p data-placement="top" data-toggle="tooltip" title="Sell"><a href="#" class="edit-modal btn btn-warning btn-xs" data-id="{{$view->id}}" data-toggle="modal" data-target="#sellModal{{$view->id}}"><i class="fas fa-dollar-sign"></a></td>
+                    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><a href="#" class="show-modal btn btn-danger btn-xs" data-id="{{$view->id}}" data-toggle="modal" data-target="#deleteModal{{$view->id}}"><i class="fas fa-trash"></i></a></td>
         </div>
 	</div>
 </div>
@@ -111,49 +98,54 @@
       <div class="modal-dialog">
     <div class="modal-content">
         <div class="modal-header">
+            <div class="row justify-content-center">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span aria-hidden="true"><i class="fas fa-pencil-alt"></span></i></button>
             <h4 class="modal-title custom_align" id="Heading">Edit Your Stocks</h4>
+            </div>
         </div>
         <div class="modal-body">
-            <form class="form-horizontal" role="modal" method="post" action="{{Route('editStocks',['id',$view->id])}}">
-                @csrf
-            <div class="form-group row add">
-            <input type="hidden" id="id" name="id">
+            <form class="form-horizontal" role="modal" method="post" action="{{ action('Stock_detailController@update',['id',$view->id]) }}">
+            {{ csrf_field() }}
+             {{ method_field('PUT') }}
+            <div class="form-group row add justify-content-center">
 
-            <input class="form-control" type="text" id="name" name="stock_name" placeholder="Stock Name" pattern="/^\p{L}+$/u">
-            {{--<!-- //<p class="error text-center alert alert-danger hidden"></p> -->--}}
+            <input class="form-control" type="text" id="name" name="stock_name" placeholder="Stock Name" pattern="/^\p{L}+$/u" style="width: 80%;">
+            {{-- <!-- <p class="error text-center alert alert-danger hidden"></p>  -->--}}
         </div>
-        <div class="form-group">
-            <input class="form-control" type="text" id="quantity" name="buy_stock_quantity" placeholder="Stock Quantity" pattern="^[0-9]+$">
-            {{--<!--<p class="error text-center alert alert-danger hidden"></p>-->--}}
+        <div class="form-group row justify-content-center">
+            <input class="form-control" type="number" id="quantity" name="buy_stock_quantity" placeholder="Stock Quantity" pattern="^[0-9]+$" style="width: 80%;">
+            {{--<!-- <p class="error text-center alert alert-danger hidden"></p> -->--}}
         </div>
-        <div class="form-group">
-            <input class="form-control" type="text" id="price" name="buy_stock_price" placeholder="Purchase Stock Price" pattern="^\s*(?=.*[1-9])\d*(?:\.\d{1,2})?\s*$">   
-            {{--<!--<p class="error text-center alert alert-danger hidden"></p>-->--}}
+        <div class="form-group row justify-content-center">
+            <input class="form-control" type="number" id="price" name="buy_stock_price" placeholder="Purchase Stock Price"  style="width: 80%;">   
+            {{--<!-- <p class="error text-center alert alert-danger hidden"></p> -->--}}
         </div>
-        <div class="form-group">
-            <input class="form-control" type="text" id="sell_quantity" name="sell_stock_quantity" placeholder="Sell Stock Quantity" pattern="^[0-9]+$">   
-            {{--<!--<p class="error text-center alert alert-danger hidden"></p>-->--}}
+        <div class="form-group row justify-content-center">
+            <input class="form-control" type="number" id="sell_quantity" name="sell_stock_quantity" placeholder="Sell Stock Quantity" pattern="^[0-9]+$" style="width: 80%;">   
+            {{--<!-- <p class="error text-center alert alert-danger hidden"></p> -->--}}
         </div>
-        <div class="form-group">
-            <input class="form-control" type="text" id="sell_price" name="sell_stock_price" placeholder="Sell Stock Price" pattern="^\s*(?=.*[1-9])\d*(?:\.\d{1,2})?\s*$">   
-            {{--<!--<p class="error text-center alert alert-danger hidden"></p>-->--}}
+        <div class="form-group row justify-content-center">
+            <input class="form-control" type="number" id="sell_price" name="sell_stock_price" placeholder="Sell Stock Price"  style="width: 80%;">   
+            {{--<!-- <p class="error text-center alert alert-danger hidden"></p> -->--}}
         </div>
-        <div class="custom-control custom-radio">
-            <input type="radio" class="custom-control-input" id="defaultradio" name="nse_or_bse" value="NSE" checked>
-            <label class="custom-control-label  col-sm-6" for="defaultChecked"> National Stock Exchange </label>
+        <div class="row justify-content-center">
+
+            <div class="custom-control custom-radio ">
+                <input type="radio" class="custom-control-input" id="defaultradio" name="nse_or_bse" value="NSE" checked>
+                <label class="custom-control-label  col-sm-4" for="defaultChecked"> NSE </label>
+            </div>
+            <div class="custom-control custom-radio ">
+                <input type="radio" class="custom-control-input" id="defaultradio" name="nse_or_bse" value="BSE">
+                <label class="custom-control-label  col-sm-4" for="defaultUnchecked"> BSE </label>
+            </div>
         </div>
-        <div class="custom-control custom-radio">
-            <input type="radio" class="custom-control-input" id="defaultradio" name="nse_or_bse" value="BSE">
-            <label class="custom-control-label  col-sm-6" for="defaultUnchecked"> Bombay Stock Exchange </label>
-        </div>
-        </form>
+       
 
         </div>   
             <div class="modal-footer">
                 <button type="submit" id="update" class="btn btn-warning btn-lg" style="width: 40%;"><i class="fas fa-check"></i> Update</button>
             </div>
-        
+        </form>
         
     <!-- modal-content --> 
   </div>
@@ -195,7 +187,10 @@
     </form> -->--}}
     <!-- ----------------------------Delete---------------------------------- -->
     <div class="modal fade" id="deleteModal{{$view->id}}" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-     <form action="{{ url('dashboard/delete/'.$view->id) }}" method="post" id="form1"> 
+     <form action="{{ action('Stock_detailController@destroy',['id'=>$view->id]) }}" method="post" id="form1"> 
+             {{ csrf_field() }}
+             {{ method_field('DELETE') }}
+         {{--<!-- url('dashboard/delete/'.$view->id) -->--}}
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -206,13 +201,14 @@
                     <div class="alert alert-danger"><i class="fas fa-exclamation fa-2x"></i> Are you sure you want to delete this Record?</div>       
                     <span class="hidden id"></span>
                     
-                    @csrf
-                    <input type="submit" class="btn btn-danger" value="YES" onclick="document.getElementById('form1').submit();">
-                 
+
+                   
+                    
                 
                 </div>
                 <div class="modal-footer ">
-                   <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fas fa-eraser"></i> No</button>
+                    <input type="submit" class="btn btn-danger" value="YES"> 
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fas fa-eraser"></i> No</button>
                 
                   
                 </div>
