a7fd75099cab3a9df641533c7082beb0040490b5
abbbhucho <abcbhucho@gmail.com>
2019-07-12 16:46:57 +0530

    new 12/7

--- app/Http/Controllers/ChargeController.php
+++ app/Http/Controllers/ChargeController.php
@@ -37,28 +37,55 @@ class ChargeController extends Controller
     {
         $usr = Auth::user();
             $id  = $request['nse_or_bse']+1;
-            $charges = Charge::find($id);
-            $charges->id = $id;
-            $charges->intra_buy_sd_percent = $request['intra_buy_sd_percent'];
-            $charges->del_buy_sd_percent = $request['del_buy_sd_percent'];
-            $charges->intra_buy_stt_percent = $request['intra_buy_stt_percent'];
-            $charges->del_buy_stt_percent = $request['del_buy_stt_percent'];
-            $charges->intra_buy_b_percent = $request['intra_buy_b_percent'];
-            $charges->del_buy_b_percent = $request['del_buy_b_percent'];
-            $charges->buy_gst_percent = $request['buy_gst_percent'];
-            $charges->intra_buy_trans_charges = $request['intra_buy_trans_charges'];
-            $charges->del_buy_trans_charges = $request['del_buy_trans_charges'];
-            $charges->intra_sell_sd_percent = $request['intra_sell_sd_percent'];
-            $charges->del_sell_sd_percent = $request['del_sell_sd_percent'];
-            $charges->intra_sell_stt_percent = $request['intra_sell_stt_percent'];
-            $charges->del_sell_stt_percent = $request['del_sell_stt_percent'];
-            $charges->intra_sell_b_percent = $request['intra_sell_b_percent'];
-            $charges->del_sell_b_percent = $request['del_sell_b_percent'];
-            $charges->sell_gst_percent = $request['sell_gst_percent'];
-            $charges->intra_sell_trans_charges = $request['intra_sell_trans_charges'];
-            $charges->del_sell_trans_charges = $request['del_sell_trans_charges'];
-            $charges->save();
+            $result = Charge::where('id',$id)->first();
+            if (!$result->isEmpty())
+             {
+                $charges = Charge::find($id);
+                $charges->id = $id;
+                $charges->intra_buy_sd_percent = $request['intra_buy_sd_percent'];
+                $charges->del_buy_sd_percent = $request['del_buy_sd_percent'];
+                $charges->intra_buy_stt_percent = $request['intra_buy_stt_percent'];
+                $charges->del_buy_stt_percent = $request['del_buy_stt_percent'];
+                $charges->intra_buy_b_percent = $request['intra_buy_b_percent'];
+                $charges->del_buy_b_percent = $request['del_buy_b_percent'];
+                $charges->buy_gst_percent = $request['buy_gst_percent'];
+                $charges->intra_buy_trans_charges = $request['intra_buy_trans_charges'];
+                $charges->del_buy_trans_charges = $request['del_buy_trans_charges'];
+                $charges->intra_sell_sd_percent = $request['intra_sell_sd_percent'];
+                $charges->del_sell_sd_percent = $request['del_sell_sd_percent'];
+                $charges->intra_sell_stt_percent = $request['intra_sell_stt_percent'];
+                $charges->del_sell_stt_percent = $request['del_sell_stt_percent'];
+                $charges->intra_sell_b_percent = $request['intra_sell_b_percent'];
+                $charges->del_sell_b_percent = $request['del_sell_b_percent'];
+                $charges->sell_gst_percent = $request['sell_gst_percent'];
+                $charges->intra_sell_trans_charges = $request['intra_sell_trans_charges'];
+                $charges->del_sell_trans_charges = $request['del_sell_trans_charges'];
+                $charges->save();
            
+             }
+             else{
+                $result->id = $id;
+                $result->intra_buy_sd_percent = $request['intra_buy_sd_percent'];
+                $result->del_buy_sd_percent = $request['del_buy_sd_percent'];
+                $result->intra_buy_stt_percent = $request['intra_buy_stt_percent'];
+                $result->del_buy_stt_percent = $request['del_buy_stt_percent'];
+                $result->intra_buy_b_percent = $request['intra_buy_b_percent'];
+                $result->del_buy_b_percent = $request['del_buy_b_percent'];
+                $result->buy_gst_percent = $request['buy_gst_percent'];
+                $result->intra_buy_trans_charges = $request['intra_buy_trans_charges'];
+                $result->del_buy_trans_charges = $request['del_buy_trans_charges'];
+                $result->intra_sell_sd_percent = $request['intra_sell_sd_percent'];
+                $result->del_sell_sd_percent = $request['del_sell_sd_percent'];
+                $result->intra_sell_stt_percent = $request['intra_sell_stt_percent'];
+                $result->del_sell_stt_percent = $request['del_sell_stt_percent'];
+                $result->intra_sell_b_percent = $request['intra_sell_b_percent'];
+                $result->del_sell_b_percent = $request['del_sell_b_percent'];
+                $result->sell_gst_percent = $request['sell_gst_percent'];
+                $result->intra_sell_trans_charges = $request['intra_sell_trans_charges'];
+                $result->del_sell_trans_charges = $request['del_sell_trans_charges'];
+                $result->save();
+             }
+            
             return back()->with('status', 'Charges updated!');
         
     }
