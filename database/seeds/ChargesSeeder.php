a26ff8b19e83384ab4bee707c64f94cbcf210231
abbbhucho <abcbhucho@gmail.com>
2019-12-06 21:48:13 +0530

    added seeder for dummy charges

---
+++ database/seeds/ChargesSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ChargesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('charges')->insert([
+            'intra_buy_sd_percent' => 0.00200,
+            'del_buy_sd_percent' => 0.01000,
+            'intra_buy_stt_percent' => 0.00100,
+            'del_buy_stt_percent'=> 0.02500,
+            'intra_buy_b_percent'=> 0.02500,
+            'del_buy_b_percent'=> 18.00000,
+            'buy_gst_percent'=> 0.00190,
+            'intra_buy_trans_charges'=> 0.01000,
+            'del_buy_trans_charges'=> 0.01000,
+            'intra_sell_sd_percent'=> 0.00200,
+            'del_sell_sd_percent'=> 0.00700,
+            'intra_sell_stt_percent'=> 0.01700,
+            'del_sell_stt_percent'=> 0.05000,
+            'intra_sell_b_percent'=> 0.02500,
+            'del_sell_b_percent'=> 0.05000,
+            'sell_gst_percent'=> 18.00000,
+            'intra_sell_trans_charges'=> 0.00190,
+            'del_sell_trans_charges'=> 0.00300,
+        ]);
+        DB::table('charges')->insert([
+            'intra_buy_sd_percent' => 0.00200,
+            'del_buy_sd_percent' => 0.00500,
+            'intra_buy_stt_percent' => 0.02500,
+            'del_buy_stt_percent'=> 0.03000,
+            'intra_buy_b_percent'=> 0.02500,
+            'del_buy_b_percent'=> 18.00000,
+            'buy_gst_percent'=> 0.00190,
+            'intra_buy_trans_charges'=> 0.01000,
+            'del_buy_trans_charges'=> 0.01000,
+            'intra_sell_sd_percent'=> 0.00200,
+            'del_sell_sd_percent'=> 0.00700,
+            'intra_sell_stt_percent'=> 0.01700,
+            'del_sell_stt_percent'=> 0.05000,
+            'intra_sell_b_percent'=> 0.02500,
+            'del_sell_b_percent'=> 0.05000,
+            'sell_gst_percent'=> 18.00000,
+            'intra_sell_trans_charges'=> 0.00190,
+            'del_sell_trans_charges'=> 0.00300,
+        ]);
+    }
+}
--- vendor/symfony/mime/RawMessage.php
+++ vendor/symfony/mime/RawMessage.php
@@ -11,10 +11,10 @@
 
 namespace Symfony\Component\Mime;
 
+use Symfony\Component\Mime\Exception\LogicException;
+
 /**
  * @author Fabien Potencier <fabien@symfony.com>
- *
- * @experimental in 4.3
  */
 class RawMessage implements \Serializable
 {
@@ -54,9 +54,16 @@ class RawMessage implements \Serializable
     }
 
     /**
+     * @throws LogicException if the message is not valid
+     */
+    public function ensureValidity()
+    {
+    }
+
+    /**
      * @internal
      */
-    final public function serialize()
+    final public function serialize(): string
     {
         return serialize($this->__serialize());
     }
@@ -69,17 +76,11 @@ class RawMessage implements \Serializable
         $this->__unserialize(unserialize($serialized));
     }
 
-    /**
-     * @internal
-     */
     public function __serialize(): array
     {
         return [$this->message];
     }
 
-    /**
-     * @internal
-     */
     public function __unserialize(array $data): void
     {
         [$this->message] = $data;
